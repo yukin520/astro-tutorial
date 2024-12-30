4b07fe85b0e563db04836b25f43b46d4587d77d2
Yuki Noda <yuki.handball@icloud.com>
2024-12-30 17:42:06 +0900

    installed preact.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://yukin520-astro-tutorial.netlify.app/"
+  site: "https://yukin520-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
